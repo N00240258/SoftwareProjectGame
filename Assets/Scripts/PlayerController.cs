efbb6e2913aca0f47c9dafcb656c790180f0b4f6
N00240258 <N00240258@iadt.ie>
2026-02-23 13:19:52 +0000

    third person movement testing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour {
 
     private GroundController _groundController;
+    //private UnityEngine.Vector3 _rotatedVector;
     PlayerInput playerInput;
     InputAction moveAction;
     [SerializeField] float speed = 5;
@@ -24,16 +25,25 @@ public class PlayerController : MonoBehaviour {
 
     void Update()
     {
-        
-        MovePlayer();
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+
+        transform.Translate(xValue,0 ,zValue);
+
+
+        //MovePlayer();
         
     }
 
     void MovePlayer()
     {
         UnityEngine.Vector3 direction = moveAction.ReadValue<UnityEngine.Vector3>();
-        mover = new UnityEngine.Vector3(direction.x, 0, direction.y) * speed * transform.localRotation.eulerAngles.y * Time.deltaTime;
-        transform.position += UnityEngine.Quaternion.AngleAxis(transform.localRotation.eulerAngles.y, mover);
+        var rotationAngle = transform.localRotation.eulerAngles.y;
+        transform.rotation = UnityEngine.Quaternion.Euler(UnityEngine.Vector3.up, 0);
+        transform.position +=  new UnityEngine.Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime;
+
+
+        //_rotatedVector = UnityEngine.Quaternion.AngleAxis(rotationAngle, Vector3.up) * speed;
         if(_groundController.isGrounded){
             transform.position += new UnityEngine.Vector3(0, direction.z * jump , 0) * speed * Time.deltaTime;
         }
