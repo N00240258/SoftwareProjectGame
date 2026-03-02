1fbe8d37128480d0b6e2c144c7996fdae720e433
N00240258 <N00240258@iadt.ie>
2026-03-02 14:00:27 +0000

    interacting with objects started

--- Assets/Scripts/interactionSystem/Interactor.cs
+++ Assets/Scripts/interactionSystem/Interactor.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Interactor : MonoBehaviour
@@ -6,15 +9,17 @@ public class Interactor : MonoBehaviour
     [SerializeField] private float _interactionPointRadius = 0.5f;
     [SerializeField] private LayerMask _interactableMask;
 
-    private readonly Collider[] _colliders = new Collider[3];
+    private readonly Collider[] _colliders = new Collider[100];
     [SerializeField] private int _numFound;
+    
+    
 
-    private void update()
+    void Update()
     {
         _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, _interactableMask);
     }
     
-    private void OnDrawGizmos()
+    void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(_interactionPoint.position, _interactionPointRadius);
