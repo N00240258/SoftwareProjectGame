9f0ac4c99361d28c111711c61e24e476d92979de
N00240258 <N00240258@iadt.ie>
2026-03-06 12:41:45 +0000

    trash goes poof on collection

--- Assets/Scripts/interactionSystem/Trash.cs
+++ Assets/Scripts/interactionSystem/Trash.cs
@@ -1,22 +1,30 @@
 using UnityEngine;
+using System.Collections;
 
 public class Trash : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _prompt;
-    [SerializeField] private int _trashSize;
+    [SerializeField] private int _trashSize = 1;
+    [SerializeField] private ParticleSystem particleSystem;
 
     public string InteractionPrompt => _prompt;
     public bool Interact(Interactor interactor)
     {
         var inventory = interactor.GetComponent<Inventory>();
 
-
         if (inventory == null) return false;
 
         if(inventory.inventorySpace + _trashSize <= inventory.inventorySize){
+
+            particleSystem.Play();
             inventory.inventorySpace += _trashSize;
             Debug.Log("Picking up! "+ inventory.inventorySpace);
-            Destroy(gameObject);
+            
+            gameObject.layer = 0;
+            gameObject.GetComponent<Collider>().isTrigger = true;
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+
+            Destroy(gameObject, 1);
             return true;
         }
         
